d1dba5f13b2282d22e5f56d6b892ba43abf660cf
Daniela Botelho <daniela.botelho@venturus.org.br>
2023-01-13 12:01:23 -0300

    SEO componets e estrutur da pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,46 @@
-import { GetServerSideProps } from 'next';
+import SEO from '../components/SEO';
+import styles from '../styles/home.module.scss';
 
-interface Post {
-  id: string;
-  title: string;
-}
-interface HomeProps {
-  posts: Post[];
-}
-
-export default function Home({ posts }: HomeProps) {
-  //ClientSide
-  // const [posts, setPosts] = useState<Post[]>([]);
-  // useEffect(() => {
-  //   fetch('http://localhost:8080/posts').then(response => {
-  //     response.json().then(data => {
-  //       setPosts(data);
-  //     });
-  //   });
-  // }, []);
-
+export default function Home() {
   return (
-    <div>
-      <h1>Posts</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title} </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <SEO title="Dev News!" excludeTitleSuffix />
+      <main className={styles.content}>
+        <section className={styles.section}>
+          <span>Olá Dev!</span>
+          <h1>
+            Bem-vindo e bem-vinda <br />
+            ao <span>Dev</span>News!
+          </h1>
+          <p>
+            Um blog com conteúdos extremamente <br />
+            <span>relevantes para o seu aprendizado.</span>
+          </p>
+        </section>
+        <img src="/home.svg" alt="Home image" />
+      </main>
+    </>
   );
 }
 
-//ServerSide
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const res = await fetch('http://localhost:8080/posts');
-  const posts = await res.json();
+//ClientSide
+// const [posts, setPosts] = useState<Post[]>([]);
+// useEffect(() => {
+//   fetch('http://localhost:8080/posts').then(response => {
+//     response.json().then(data => {
+//       setPosts(data);
+//     });
+//   });
+// }, []);
 
-  return {
-    props: {
-      posts,
-    },
-  };
-};
+//ServerSide
+// export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+//   const res = await fetch('http://localhost:8080/posts');
+//   const posts = await res.json();
+
+//   return {
+//     props: {
+//       posts,
+//     },
+//   };
+// };
