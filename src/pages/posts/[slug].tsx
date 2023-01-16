95be439e226e306e5d2b734d7ba09f676914cbb8
Daniela Botelho <daniela.botelho@venturus.org.br>
2023-01-16 15:07:27 -0300

    conectando api Prismic a aplicacao

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,92 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import SEO from '../../components/SEO';
+import { RichText } from 'prismic-dom';
+import { getPrismicClient } from '../../services/prismic';
+import styles from './post.module.scss';
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
+}
+
+interface PrismicDocumentTitle {
+  type: string;
+  text: string;
+}
+
+interface PrismicDocumentContent {
+  type: string;
+  text: string;
+}
+
+interface PrismicDocument {
+  data: {
+    title: PrismicDocumentTitle[];
+    content: PrismicDocumentContent[];
+  };
+}
+
+export default function Post({ post }: PostProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <SEO title="Post" />
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={styles.content}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const { slug } = context.params;
+
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getByUID('post', String(slug), {});
+  const document = response as PrismicDocument;
+
+  const post = {
+    slug,
+    title: RichText.asText(document.data.title),
+    content: RichText.asText(document.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      'pt-BR',
+      {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      },
+    ),
+  };
+
+  return {
+    props: { post },
+    revalidate: 60 * 60 * 12, // 12 horas
+  };
+};
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,29 +1,56 @@
 import { GetStaticProps } from 'next';
-import SEO from '../../components/SEO';
+import Prismic from '@prismicio/client';
 import Link from 'next/link';
+import SEO from '../../components/SEO';
+import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom';
 import styles from './posts.module.scss';
 
 interface Post {
-  id: string;
+  slug: string;
   title: string;
+  excerpt: string;
+  updateAt: string;
 }
+
 interface PostsProps {
   posts: Post[];
 }
 
-export default function Posts() {
+interface PrismicDocumentTitle {
+  type: string;
+  text: string;
+}
+
+interface PrismicDocumentContent {
+  type: string;
+  text: string;
+}
+
+interface PrismicDocument {
+  uid: string;
+  last_publication_date: string;
+  data: {
+    title: PrismicDocumentTitle[];
+    content: PrismicDocumentContent[];
+  };
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <SEO title="Posts" />
       <main className={styles.container}>
         <div className={styles.posts}>
-          <Link href="#">
-            <a>
-              <time>25 de dezembro de 2021</time>
-              <strong>Titulo</strong>
-              <p>Paragrafo</p>
-            </a>
-          </Link>
+          {posts.map(post => (
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time>{post.updateAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
@@ -31,8 +58,38 @@ export default function Posts() {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+    },
+  );
+
+  const posts = response.results.map(post => {
+    const document = post as PrismicDocument;
+    return {
+      slug: post.uid,
+      title: RichText.asText(document.data.title),
+      excerpt:
+        document.data.content.find(content => content.type === 'paragraph')
+          ?.text ?? '',
+      updateAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        },
+      ),
+    };
+  });
+
   return {
-    props: {},
+    props: {
+      posts,
+    },
     revalidate: 60 * 60 * 12, // 12 HORAS
   };
 };
